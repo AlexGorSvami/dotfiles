dd26809599fd6eab48540f0a5134b36c81a0f915
AlexGorSvami <jagger15696@gmail.com>
2026-04-30 15:10:05 +0500

    Final Nvim 0.11 config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,204 +1,140 @@
 -- ==============================================================================
--- БЛОК 1: Инициализация пакетного менеджера (Lazy)
+-- 1. СИСТЕМНЫЕ НАСТРОЙКИ (Manjaro Linux, 24h формат)
 -- ==============================================================================
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- ==============================================================================
--- БЛОК 2: Базовые настройки редактора
--- ==============================================================================
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
+-- Русская раскладка (навигация без переключения языка)
+vim.opt.langmap = 'ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz'
+
+vim.opt.number = true           -- Номера строк
+vim.opt.relativenumber = true   -- Относительные номера
+vim.opt.shiftwidth = 4          -- Табуляция 4 пробела
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus" -- Системный буфер (требуется xclip)
+vim.opt.mouse = "a"
+vim.opt.undofile = true         -- Сохранение истории
 
 -- ==============================================================================
--- БЛОК 3: Установка плагинов
+-- 2. ПЛАГИНЫ (Lazy.nvim)
 -- ==============================================================================
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup({
-    -- 3.1 Внешний вид (Kanagawa с полной прозрачностью)
-  {
-    "rebelot/kanagawa.nvim",
-    priority = 1000,
-    config = function()
-      require("kanagawa").setup({
-        transparent = true, -- Сохраняем прозрачность терминала
-        theme = "wave",     -- Основной современный профиль
-      })
+  -- Тема Kanagawa и полная прозрачность
+  { "rebelot/kanagawa.nvim", priority = 1000, config = function()
+      require("kanagawa").setup({ transparent = true, theme = "wave" })
       vim.cmd("colorscheme kanagawa")
-      
-      -- Принудительно убираем фон у колонки с номерами строк
-      vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-      vim.api.nvim_set_hl(0, "LineNrAbove", { bg = "none" })
-      vim.api.nvim_set_hl(0, "LineNrBelow", { bg = "none" })
-      vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+      -- Принудительная прозрачность всех панелей
+      local groups = { "Normal", "NormalNC", "LineNr", "SignColumn", "StatusLine", "EndOfBuffer" }
+      for _, group in ipairs(groups) do vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" }) end
+    end 
+  },
+
+  -- Удобство и Визуал
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }, -- Линии отступов
+  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }, config = true },
+  { "nvim-neo-tree/neo-tree.nvim", branch = "v3.x", dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" } },
+  { "nvim-telescope/telescope.nvim", tag = "0.1.5", dependencies = { "nvim-lua/plenary.nvim" } },
+
+  -- Инструменты разработки
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = function()
+      require("nvim-treesitter.configs").setup({ ensure_installed = { "python", "pascal", "lua", "c" }, highlight = { enable = true } })
     end
   },
-    -- 3.2 Файловое дерево и Поиск (Telescope)
-  { 
-    "nvim-neo-tree/neo-tree.nvim", 
-    branch = "v3.x", 
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" } 
-  },
-  { 
-    "nvim-telescope/telescope.nvim", 
-    tag = "0.1.5", 
-    dependencies = { "nvim-lua/plenary.nvim" } 
-  },
+  { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+  { "numToStr/Comment.nvim", config = true },
 
-  -- 3.3 Подсветка синтаксиса (Treesitter)
-  { 
-    "nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate", 
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "python", "pascal", "lua", "c", "kotlin" },
-        highlight = { enable = true },
-      })
-    end
-  },
-
-  -- 3.4 Управление серверами (Mason)
-  { "williamboman/mason.nvim", config = true },
-
-  -- 3.5 Автодополнение и интеграция LSP (включая Luasnip)
-  { 
-    "hrsh7th/nvim-cmp", 
-    dependencies = { 
-      "hrsh7th/cmp-nvim-lsp", 
-      "hrsh7th/cmp-buffer",
-      "L3MON4D3/LuaSnip", 
-      "saadparwaiz1/cmp_luasnip" 
+  -- LSP, Сниппеты и Автодополнение
+  { "neovim/nvim-lspconfig" },
+  { "hrsh7th/nvim-cmp", dependencies = { 
+      "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" 
     } 
   },
 
-  -- 3.6 Отладчик (DAP)
-  { 
-    "mfussenegger/nvim-dap", 
-    dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" } 
-  },
+  -- Отладка (DAP)
+  { "mfussenegger/nvim-dap", dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" } },
   { "mfussenegger/nvim-dap-python" },
 })
 
 -- ==============================================================================
--- БЛОК 4: Автодополнение и LSP (Новый API Neovim)
+-- 3. LSP (Native API Neovim 0.11+)
+-- ==============================================================================
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Pascal (pasls - настраиваем вручную, как просил)
+vim.lsp.config('pasls', { cmd = { "pasls" }, capabilities = capabilities, filetypes = { "pascal", "pp", "inc" } })
+vim.lsp.enable('pasls')
+
+-- Остальные (Python, C)
+local servers = { "pyright", "clangd" }
+for _, s in ipairs(servers) do
+    vim.lsp.config(s, { capabilities = capabilities })
+    vim.lsp.enable(s)
+end
+
+-- ==============================================================================
+-- 4. ИСПРАВЛЕННЫЙ CMP (Автодополнение и ТАБ)
 -- ==============================================================================
 local cmp = require('cmp')
 local luasnip = require('luasnip')
 
--- Настройка движка автодополнения
+-- Автоскобки после выбора функции
+cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
+
 cmp.setup({
-  snippet = {
-    expand = function(args) luasnip.lsp_expand(args.body) end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-Space>'] = cmp.mapping.complete(),
+  snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
+  mapping = {
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  }, {
-    { name = 'buffer' },
-  })
-})
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
--- Инициализация серверов
-local servers = { "pyright", "pasls", "clangd", "kotlin_language_server" }
-for _, server in ipairs(servers) do
-    vim.lsp.config(server, {
-        capabilities = capabilities,
-    })
-    vim.lsp.enable(server)
-end
-
--- Автоматические проверки (Diagnostics)
-vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  update_in_insert = false,
-  underline = true,
+    -- Жесткая логика ТАБА
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  },
+  sources = cmp.config.sources({ { name = 'nvim_lsp' }, { name = 'luasnip' }, { name = 'path' } }, { { name = 'buffer' } })
 })
 
 -- ==============================================================================
--- БЛОК 5: Настройка Отладчика (DAP)
+-- 5. ОТЛАДКА (DAP) И ГОРЯЧИЕ КЛАВИШИ
 -- ==============================================================================
+require('dap-python').setup('python')
 local dap = require('dap')
 
--- Настройка DAP для Kotlin
-dap.adapters.kotlin = {
-    type = "executable",
-    command = "kotlin-debug-adapter",
-    args = {}
-}
-
-dap.configurations.kotlin = {
-    {
-        type = "kotlin",
-        request = "launch",
-        name = "Launch Kotlin Program",
-        mainClass = function()
-            return vim.fn.input('Main class: ')
-        end,
-        projectRoot = "${workspaceFolder}",
-    }
-}
-
--- Инициализация DAP для Python
-require('dap-python').setup('python')
-
--- Настройка DAP для C
-dap.adapters.gdb = {
-    type = "executable",
-    command = "gdb",
-    name = "gdb"
-}
-
+-- Настройка GDB для C и Pascal
+dap.adapters.gdb = { type = "executable", command = "gdb", name = "gdb" }
 dap.configurations.c = {
-    {
-        name = "Launch C Program",
-        type = "gdb",
-        request = "launch",
-        program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = "${workspaceFolder}",
-    }
+  { name = "Launch", type = "gdb", request = "launch", program = function() return vim.fn.input('Path: ', vim.fn.getcwd() .. '/', 'file') end, cwd = "${workspaceFolder}" }
 }
+dap.configurations.pascal = dap.configurations.c
 
--- ==============================================================================
--- БЛОК 6: Горячие клавиши
--- ==============================================================================
-local builtin = require('telescope.builtin')
-
--- Интерфейс
-vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = 'Toggle Neo-tree' })
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find Files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live Grep' })
-
--- Компиляция и запуск
-vim.keymap.set('n', '<F6>', ':w<CR>:!fpc % && ./%:r<CR>', { desc = 'Run Pascal' })
-
--- Управление отладкой
-vim.keymap.set('n', '<F5>', function() require('dap').continue() end, { desc = 'DAP: Start/Continue' })
-vim.keymap.set('n', '<F10>', function() require('dap').step_over() end, { desc = 'DAP: Step Over' })
-vim.keymap.set('n', '<F11>', function() require('dap').step_into() end, { desc = 'DAP: Step Into' })
-vim.keymap.set('n', '<leader>b', function() require('dap').toggle_breakpoint() end, { desc = 'DAP: Toggle Breakpoint' })
+-- КЛАВИШИ
+vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>')
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').find_files)
+vim.keymap.set('n', '<F6>', ':w<CR>:!fpc % && ./%:r<CR>') -- Pascal
+vim.keymap.set('n', '<F5>', function() dap.continue() end)
+vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+vim.keymap.set('n', '<F11>', function() dap.step_into() end)
+vim.keymap.set('n', '<leader>b', function() dap.toggle_breakpoint() end)
